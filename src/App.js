822079adce0fb2a2346fb6c99d5cfe5fad4b3a80
fifi-1998 <ferielmejri14@gmail.com>
2023-01-31 11:12:18 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Form from "./componment/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      {" "}
+      <Form />
+    </div>
+  );
+}
+
+export default App;
